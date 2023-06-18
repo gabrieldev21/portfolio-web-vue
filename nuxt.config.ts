5b84bba7912299ed03c7e8ec11a3878c4dfdc2f9
gabrieldev21 <gabrielp.amorim@live.com>
2023-06-17 22:09:28 -0300

    feat: config tailwinds and css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
+import { resolve } from "path";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
-})
+  // modules: ["@nuxt/content"],
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: ["~/style/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+});
